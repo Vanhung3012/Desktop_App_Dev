d4cfb7abac066667f2cac2c8ef963700f5d5ceb6
Hồ Văn Hưng <113756033+Vanhung3012@users.noreply.github.com>
2023-03-28 10:51:29 +0700

    Add files via upload

---
+++ BaiTapBuoiLyThuyetSo2/BaiTapBuoiLyThuyetSo2/frmMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace BaiTapBuoiLyThuyetSo2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.FrmMain = new System.Windows.Forms.MenuStrip();
+            this.hồVănHưngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.frm35ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.frm42ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.frm48ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.frm52ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.frm56ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.frm57ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.frm60ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FrmMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FrmMain
+            // 
+            this.FrmMain.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.FrmMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hồVănHưngToolStripMenuItem});
+            this.FrmMain.Location = new System.Drawing.Point(0, 0);
+            this.FrmMain.Name = "FrmMain";
+            this.FrmMain.Size = new System.Drawing.Size(800, 31);
+            this.FrmMain.TabIndex = 0;
+            this.FrmMain.Text = "menuStrip1";
+            // 
+            // hồVănHưngToolStripMenuItem
+            // 
+            this.hồVănHưngToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.frm35ToolStripMenuItem,
+            this.frm42ToolStripMenuItem,
+            this.frm48ToolStripMenuItem,
+            this.frm52ToolStripMenuItem,
+            this.frm56ToolStripMenuItem,
+            this.frm57ToolStripMenuItem,
+            this.frm60ToolStripMenuItem});
+            this.hồVănHưngToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.hồVănHưngToolStripMenuItem.Name = "hồVănHưngToolStripMenuItem";
+            this.hồVănHưngToolStripMenuItem.Size = new System.Drawing.Size(131, 27);
+            this.hồVănHưngToolStripMenuItem.Text = "Hồ Văn Hưng";
+            // 
+            // frm35ToolStripMenuItem
+            // 
+            this.frm35ToolStripMenuItem.Name = "frm35ToolStripMenuItem";
+            this.frm35ToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.frm35ToolStripMenuItem.Text = "frm35";
+            this.frm35ToolStripMenuItem.Click += new System.EventHandler(this.frm35ToolStripMenuItem_Click);
+            // 
+            // frm42ToolStripMenuItem
+            // 
+            this.frm42ToolStripMenuItem.Name = "frm42ToolStripMenuItem";
+            this.frm42ToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.frm42ToolStripMenuItem.Text = "frm42";
+            this.frm42ToolStripMenuItem.Click += new System.EventHandler(this.frm42ToolStripMenuItem_Click);
+            // 
+            // frm48ToolStripMenuItem
+            // 
+            this.frm48ToolStripMenuItem.Name = "frm48ToolStripMenuItem";
+            this.frm48ToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.frm48ToolStripMenuItem.Text = "frm48";
+            this.frm48ToolStripMenuItem.Click += new System.EventHandler(this.frm48ToolStripMenuItem_Click);
+            // 
+            // frm52ToolStripMenuItem
+            // 
+            this.frm52ToolStripMenuItem.Name = "frm52ToolStripMenuItem";
+            this.frm52ToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.frm52ToolStripMenuItem.Text = "frm52";
+            this.frm52ToolStripMenuItem.Click += new System.EventHandler(this.frm52ToolStripMenuItem_Click);
+            // 
+            // frm56ToolStripMenuItem
+            // 
+            this.frm56ToolStripMenuItem.Name = "frm56ToolStripMenuItem";
+            this.frm56ToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.frm56ToolStripMenuItem.Text = "frm56";
+            this.frm56ToolStripMenuItem.Click += new System.EventHandler(this.frm56ToolStripMenuItem_Click);
+            // 
+            // frm57ToolStripMenuItem
+            // 
+            this.frm57ToolStripMenuItem.Name = "frm57ToolStripMenuItem";
+            this.frm57ToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.frm57ToolStripMenuItem.Text = "frm57";
+            this.frm57ToolStripMenuItem.Click += new System.EventHandler(this.frm57ToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // frm60ToolStripMenuItem
+            // 
+            this.frm60ToolStripMenuItem.Name = "frm60ToolStripMenuItem";
+            this.frm60ToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.frm60ToolStripMenuItem.Text = "frm60";
+            this.frm60ToolStripMenuItem.Click += new System.EventHandler(this.frm60ToolStripMenuItem_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FrmMain);
+            this.MainMenuStrip = this.FrmMain;
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.FrmMain.ResumeLayout(false);
+            this.FrmMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip FrmMain;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hồVănHưngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem frm35ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem frm42ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem frm48ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem frm52ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem frm56ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem frm57ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem frm60ToolStripMenuItem;
+    }
+}
---
+++ BaiTapBuoiLyThuyetSo2/BaiTapBuoiLyThuyetSo2/frmMain.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapBuoiLyThuyetSo2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frm35ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new frm35();
+            form.ShowDialog();
+        }
+
+        private void frm42ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new frm42();
+            form.ShowDialog();
+        }
+
+        private void frm48ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new frm48();
+            form.ShowDialog();
+        }
+
+        private void frm52ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new frm52();
+            form.ShowDialog();
+        }
+
+        private void frm56ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new frm56();
+            form.ShowDialog();
+        }
+
+        private void frm57ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new frm57();
+            form.ShowDialog();
+        }
+
+        private void frm60ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new frm60();
+            form.ShowDialog();
+        }
+    }
+}
